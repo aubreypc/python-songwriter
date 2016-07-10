f6bc76ddc5d4070f88650a8d424364db78b9489a
Aubrey Collins <collinsp@allegheny.edu>
2016-07-09 20:16:03 -0400

    generator for traversing tries

---
+++ tests/test_Trie.py
@@ -0,0 +1,29 @@
+import unittest
+from trie.Trie import Trie
+from trie.TrieCursor import TrieCursor
+
+class TrieTests(unittest.TestCase):
+    def test_make_child(self):
+        t = Trie(2)
+        child = t.root.make_child(1, 0)
+        self.assertTrue(t.root.links[0] is child)
+        self.assertTrue(t.root.links[0].val == 1)
+
+    def test_cursor(self):
+        t = Trie(2)
+
+        t.root.val = 1
+        node2 = t.root.make_child(2, 0)
+        node3 = node2.make_child(3, 1)
+        
+        current = t.travel(0)
+        self.assertTrue(current.val == 2)
+
+        current = t.travel(1)
+        self.assertTrue(current.val == 3)
+
+        with self.assertRaises(StopIteration):
+            t.travel(1)
+
+if __name__ == "__main__":
+    unittest.main()
--- trie/Trie.py
+++ trie/Trie.py
@@ -4,11 +4,29 @@ class Trie(object):
     def __init__(self, size):
         self.size = size
         self.root = self.TrieNode(None, size)
-        self.cursor = TrieCursor(self)
+        self.new_cursor()
+
+    def generator(self):
+        while True:
+            self.target_link = yield
+            if not self.current.has_children():
+                break
+            self.current = self.current.links[self.target_link]
+            yield self.current
+
+    def new_cursor(self):
+        self.current = self.root
+        self.cursor = self.generator()
+        return self.cursor
+
+    def travel(self, next_link):
+        self.cursor.next()
+        return self.cursor.send(next_link)
 
     class TrieNode(object):
         def __init__(self, val, size, parent=None):
             self.links = [None] * size
+            self.size = size
             self.val = val
             self.parent = parent
 
@@ -16,13 +34,13 @@ class Trie(object):
             return self.links[index]
 
         def __setitem__(self, index, link):
-            if type(link) is self.TrieNode or type(link) is None:
+            if type(link) is TrieNode or type(link) is None:
                 self.links[index] = link
             else:
                 raise Exception("TrieNode can only link to other TrieNodes or to None.")
 
         def make_child(self, val, index):
-            new = self.TrieNode(self, val, size, parent=self)
+            new = self.__class__(val, self.size, parent=self)
             self.links[index] = new
             return new
 

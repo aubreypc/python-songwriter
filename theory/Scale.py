c43f950bd19e4c0049dca124f3c5c3e49b5b0513
Aubrey Collins <collinsp@allegheny.edu>
2016-07-10 06:56:42 -0400

    rename sample scale functions

--- theory/Scale.py
+++ theory/Scale.py
@@ -5,34 +5,34 @@ class Scale(NoteGroup):
         return self.starting_with(step)
         
 
-def chromatic():
+def chromatic_scale():
     notes = Scale([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
     return notes
 
-def major():
+def major_scale():
     notes = Scale([0, 2, 4, 5, 7, 9, 11])
     return notes
 
-def ionian():
-    return major()
+def ionian_mode():
+    return major_scale()
 
-def dorian():
-    notes = major().relative_mode(self, 1)
+def dorian_mode():
+    notes = major_scale().relative_mode(self, 1)
 
-def phrygian():
-    notes = major().relative_mode(self, 2)
+def phrygian_mode():
+    notes = major_scale().relative_mode(self, 2)
 
-def lydian():
-    notes = major().relative_mode(self, 3)
+def lydian_mode():
+    notes = major_scale().relative_mode(self, 3)
 
-def mixolydian():
-    notes = major().relative_mode(self, 4)
+def mixolydian_mode():
+    notes = major_scale().relative_mode(self, 4)
 
-def aeolian():
-    notes = major().relative_mode(self, 5)
+def aeolian_mode():
+    notes = major_scale().relative_mode(self, 5)
 
-def minor():
-    return aeolian()
+def minor_scale():
+    return aeolian_mode()
 
-def locrian():
-    notes = major().relative_mode(self, 6)
+def locrian_mode():
+    notes = major_scale().relative_mode(self, 6)

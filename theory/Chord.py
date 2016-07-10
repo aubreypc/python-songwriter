2d8322e31fc3fde29785ad9567b150aa59b1c02d
Aubrey Collins <collinsp@allegheny.edu>
2016-07-10 06:41:46 -0400

    chord inversions

--- theory/Chord.py
+++ theory/Chord.py
@@ -1,5 +1,5 @@
 from NoteGroup import NoteGroup
 
 class Chord(NoteGroup):
-    def __init__(self):
-        super(self.__class__, self).__init__()
+    def inversion(self, step):
+        return self.starting_with(step)

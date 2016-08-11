46c4464fc477125feb822cc04a2749f8be51e2c5
Aubrey Collins <aubrey@aubreypc.com>
2016-08-11 16:08:22 -0400

    add modifier back after searching

--- search/Sequential.py
+++ search/Sequential.py
@@ -2,6 +2,7 @@ import Domain
 	
 def run(input, domain):
     #transpose the input to match the domain values
+    print input
     modifier = input[0].val
     input = [note - modifier for note in input]
     print input
@@ -13,7 +14,7 @@ def run(input, domain):
                 match = False
                 break
         if match:
-            solutions.append(group)
+            solutions.append(group + modifier)
             if domain == Domain.ALL_MAJOR_SCALES:
                 # add relative diatonic modes
                 modes = ["ionian (major)", "dorian", "phrygian", "lydian", "mixolydian", 
@@ -21,5 +22,5 @@ def run(input, domain):
                 for i in range(2, 7):
                     mode = group.relative_mode(i)
                     mode.name = modes[i]
-                    solutions.append(mode)
+                    solutions.append(mode + modifier)
     return solutions

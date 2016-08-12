dab2b93598211d8e76a6cc79ea42ae2f52bba509
Aubrey Collins <aubrey@aubreypc.com>
2016-08-12 00:08:43 -0400

    add more chord types

--- search/Domain.py
+++ search/Domain.py
@@ -1,8 +1,15 @@
-from theory.Scale import *
-from theory.Chord import *
+import theory.Scale as Scale
+import theory.Chord as Chord
 
 """
 Define our default search domains.
 """
 
-ALL_MAJOR_SCALES = [major_scale() + i for i in range(12)]
+ALL_MAJOR_SCALES = [Scale.major_scale() + i for i in range(12)]
+# TODO: revisit this --- we don't actually need ALL the major scales,
+# if we're modifying the input such that the root note has val 0
+TRIADS = [Chord.maj(), Chord.min(), Chord.dim(), Chord.aug()]
+SEVENTH_CHORDS = [Chord.maj7(), Chord.min7()]
+SUSPENDED_CHORDS = [Chord.sus2(), Chord.sus4()]
+
+DEFAULT_CHORDS = TRIADS + SEVENTH_CHORDS + SUSPENDED_CHORDS #I CAN'T STOP YELLING
--- theory/Chord.py
+++ theory/Chord.py
@@ -1,4 +1,5 @@
 from NoteGroup import NoteGroup
+from Note import Note
 
 class Chord(NoteGroup):
     def __init__(self, *args, **kwargs):
@@ -20,3 +21,29 @@ class Chord(NoteGroup):
         if self.root:
             s.root = Note(self.root.val + other)
         return s
+
+def maj():
+    return Chord([0, 4, 7], name="Major triad")
+
+def maj7():
+    return Chord([0, 4, 7, 11], name="Major 7")
+
+def min():
+    return Chord([0, 3, 7], name="Minor triad")
+
+def min7():
+    return Chord([0, 3, 7, 11], name="Minor 7")
+
+def dim():
+    return Chord([0, 3, 6], name="Diminished triad")
+
+def aug():
+    return Chord([0, 4, 8], name="Augmented triad")
+
+def sus2():
+    return Chord([0, 2, 7], name="Suspended 2nd")
+
+def sus4():
+    return Chord([0, 5, 7], name="Suspended 4th")
+
+

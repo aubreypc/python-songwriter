dca0bf6f993570a2f4c7c4200975a6a2f6fa8943
Aubrey Collins <aubrey@aubreypc.com>
2016-08-11 23:04:47 -0400

    parse input with argparse

--- run_search.py
+++ run_search.py
@@ -1,8 +1,19 @@
 from search import Domain, Sequential, Subset
 from theory.Note import Note
+from theory.Scale import chromatic_scale
+import argparse
+
+parser = argparse.ArgumentParser(description="Search through musical scales and chords.")
+#TODO: actually increase the verbosity
+parser.add_argument("-v", "--verbose", help="Enable verbose output", action="store_true")
+parser.add_argument("Notes", help="The group of notes to search for.", nargs="+")
 
 if __name__ == "__main__":
-    input = [Note(2), Note(3)]
+    args = parser.parse_args()
+    # convert note args to Note objs
+    chrom = map(str, chromatic_scale())
+    input = [Note(chrom.index(arg)) for arg in args.Notes]
+
     print "Running sequential:"
     results = Sequential.run(input, Domain.ALL_MAJOR_SCALES)
     for result in results:

9a4529c27a3335669e4d744dbbfa4dae268a7902
Aubrey Collins <aubrey@aubreypc.com>
2016-08-17 04:00:06 -0400

    fix duplicate results bug

--- search/Sequential.py
+++ search/Sequential.py
@@ -2,10 +2,8 @@ import Domain
 	
 def run(input, domain):
     #transpose the input to match the domain values
-    print input
     modifier = input[0].val
     input = [note - modifier for note in input]
-    print input
     solutions = []
     for group in domain:
         match = True
@@ -15,12 +13,4 @@ def run(input, domain):
                 break
         if match:
             solutions.append(group + modifier)
-            if domain == Domain.ALL_MAJOR_SCALES:
-                # add relative diatonic modes
-                modes = ["ionian (major)", "dorian", "phrygian", "lydian", "mixolydian", 
-                        "aeolian (minor)", "locrian"]
-                for i in range(2, 7):
-                    mode = group.relative_mode(i)
-                    mode.name = modes[i]
-                    solutions.append(mode + modifier)
     return solutions
